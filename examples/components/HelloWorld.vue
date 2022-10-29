af9527efe5eb280e05056cb25620b57386f10c45
曹明奇 <caomq@caomingqideMacBook-Pro.local>
2022-10-29 20:48:46 +0800

    first commit

---
+++ examples/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+   <mq-btn :loading="loading" type="success" size="medium" @click="loadClick" >查询</mq-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonUse',
+  data() {
+    return {
+      loading: false
+    };
+  },
+  methods: {
+    loadClick() {
+        this.loading = true;
+        setTimeout( () =>{
+            this.loading = false;
+        },2000)
+    }
+  }
+}
+</script>
